54c72bbafd7f35237b3d71cf0c07f5357bed6b80
Ilshat <ilsaf13@yahoo.com>
2015-10-05 14:55:49 +0300

    o

--- js/table-koshp.js
+++ js/table-koshp.js
@@ -26,7 +26,7 @@ $(document).ready(function () {
     };
     $.ajax({
         type: 'POST',
-        url: 'http://pcms.university.innopolis.ru/olympiads/values/koshp-registration-form-ViewObject',
+        url: 'https://pcms.university.innopolis.ru/olympiads/values/koshp-registration-form-ViewObject',
         success: function (response) {
             if (response) {
                 var data = eval(response);
